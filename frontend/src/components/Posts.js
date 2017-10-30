0630a4dee678fb418439e4f5e23356f1fe04332a
Ryan Holder <ryan@ryanholder.com>
2017-10-30 11:53:45 +0000

    refactor: Remove commented out code on Posts.js

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -25,42 +25,6 @@ class Posts extends Component {
     }, 0);
   }
 
-  // handleGetCommentCount = (postId) => {
-  //   const { comments } = this.props;
-  //   // let commentCount = 0;
-  //   //
-  //   // let commentCount = comments.items.reduce(function(sum, word) {
-  //   //   return sum + word.length;
-  //   // }, 0);
-  //   // console.log(total);
-  //   //
-  //   // for (let i = 0; i < this.length; i += 1) {
-  //   //   if (this[i] === query) {
-  //   //
-  //   //   }
-  //   //     count++;
-  //   //     return count;
-  //   // }
-  //   //
-  //   //   const bookInList = this.props.myBooks.filter(myBook => myBook.id === book.id)[0];
-  //   //   if (bookInList === undefined) {
-  //   //     return 'none';
-  //   //   }
-  //   //   return bookInList.shelf;
-  //   //
-  //   // const commentCount = comments.items.reduce((acc, cur) => {
-  //   //   (cur.id === postId) ? acc =+ 1 : acc;
-  //   // }, 0);
-  //
-  //   // const commentCount = comments.items.reduce((count, item) => {
-  //   //   if (item.id === postId) {
-  //   //     count += 1;
-  //   //     return count;
-  //   //   }
-  //   // }, 0);
-  //   return commentCount;
-  // }
-
   render() {
     const { posts } = this.props;
     return (
