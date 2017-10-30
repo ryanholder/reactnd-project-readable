6c9fb41e7fa53bdf6652493ea9f87cac4d285173
Ryan Holder <ryan@ryanholder.com>
2017-10-30 12:47:06 +0000

    refactor: Remove match and PropTypes from App

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PostView from './PostView';
 import PostDetailView from './PostDetailView';
 
 
-const App = ({ match }) => (
+const App = () => (
   <Router>
     <Switch>
       <Route exact path="/:category?" component={PostView} />
@@ -21,10 +20,6 @@ const mapStateToProps = (state) => {
     categories,
     posts,
   };
-};
-
-App.propTypes = {
-  match: PropTypes.array.isRequired,
 };
 
 export default connect(mapStateToProps)(App);
