a9849a59062329a8b73ce443bde63adb9a20736f
Ryan Holder <ryan@ryanholder.com>
2017-11-06 12:01:41 +0000

    feat: CreateAdd Post dialog

--- frontend/src/components/AddPost.js
+++ frontend/src/components/AddPost.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import Button from 'material-ui/Button';
+import AddPostForm from './AddPostForm';
 
-const AddPost = () => {
-  return (
-    <Button raised color="accent">
-      Add Post
-    </Button>
-  );
-};
+class AddPost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleRequestClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button
+          onClick={this.handleClickOpen}
+          raised
+          color="accent"
+        >
+          Add Post
+        </Button>
+        <AddPostForm
+          handleRequestClose={this.handleRequestClose}
+          isOpen={this.state.open}
+        />
+      </div>
+    );
+  }
+}
 
 export default AddPost;
---
+++ frontend/src/components/AddPostForm.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withStyles } from 'material-ui/styles';
+import Dialog from 'material-ui/Dialog';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import Typography from 'material-ui/Typography';
+import CloseIcon from 'material-ui-icons/Close';
+import TextField from 'material-ui/TextField';
+import MenuItem from 'material-ui/Menu/MenuItem';
+import Grid from 'material-ui/Grid';
+import Slide from 'material-ui/transitions/Slide';
+
+const styles = {
+  appBar: {
+    position: 'relative',
+  },
+  flex: {
+    flex: 1,
+  },
+};
+
+const categories = [
+  {
+    value: 'react',
+    label: 'React',
+  },
+  {
+    value: 'redux',
+    label: 'Redux',
+  },
+  {
+    value: 'udacity',
+    label: 'Udacity',
+  },
+];
+
+function Transition(props) {
+  return <Slide direction="up" {...props} />;
+}
+
+class AddPostForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      body: '',
+      author: '',
+      category: '',
+    };
+  }
+
+  handleSubmit = (event) => {
+    const { dispatch } = this.props;
+    // dispatch(addPost(this.state));
+    event.preventDefault();
+  };
+
+  handleChange = name => (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Dialog
+        fullScreen
+        open={this.props.isOpen}
+        onRequestClose={this.props.handleRequestClose}
+        transition={Transition}
+      >
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton color="contrast" onClick={this.props.handleRequestClose} aria-label="Close">
+              <CloseIcon />
+            </IconButton>
+            <Typography type="title" color="inherit" className={classes.flex}>
+              Add Post
+            </Typography>
+            <Button color="contrast" type="submit" form="addPost">
+              save
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <form id="addPost" className="grid-container" onSubmit={this.handleSubmit}>
+          <Grid container spacing={24}>
+            <Grid item xs={12}>
+              <TextField
+                value={this.state.title}
+                onChange={this.handleChange('title')}
+                label="Title"
+                margin="normal"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                value={this.state.body}
+                onChange={this.handleChange('body')}
+                label="Content"
+                margin="normal"
+                fullWidth
+                multiline
+                rows="10"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                value={this.state.author}
+                onChange={this.handleChange('author')}
+                label="Author"
+                placeholder="Name"
+                margin="normal"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                value={this.state.category}
+                onChange={this.handleChange('category')}
+                select
+                label="Select Category"
+                className="select-categories-main"
+                SelectProps={{
+                  MenuProps: {
+                    className: 'select-categories',
+                  },
+                }}
+                margin="normal"
+                fullWidth
+              >
+                {categories.map(option => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+          </Grid>
+        </form>
+      </Dialog>
+    );
+  }
+}
+
+AddPostForm.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  handleRequestClose: PropTypes.func.isRequired,
+};
+
+export default connect()(withStyles(styles)(AddPostForm));
