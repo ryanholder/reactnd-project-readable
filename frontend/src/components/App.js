0a1932f3a76344f8b9d01ad904b11610df35cc17
Ryan Holder <ryan@ryanholder.com>
2017-11-12 08:55:49 +0000

    refactor: Remove Redux connect from App

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PostView from './PostView';
 import PostDetailView from './PostDetailView';
 
-
 const App = () => (
-  <Router>
+  <BrowserRouter>
     <Switch>
       <Route exact path="/:category?" component={PostView} />
       <Route exact path="/:category/:id" component={PostDetailView} />
     </Switch>
-  </Router>
+  </BrowserRouter>
 );
 
-const mapStateToProps = (state) => {
-  const { categories, posts } = state;
-  return {
-    categories,
-    posts,
-  };
-};
-
-export default connect(mapStateToProps)(App);
+export default App;
